4fa4ac87657213ef8096b2c9b806b5597cfde077
JhonCND <68496527+JhonCND@users.noreply.github.com>
2021-03-12 19:41:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import telebot
-#token
-TOKEN = ("API_KEY")
-bot = telebot.TeleBot(TOKEN)
 
-#commands
-@bot.message_handler(commands=['start','help'])
-def send_welcome(message):
-    bot.reply_to(message, "opa, bot teste!")
+class main():
+    def _init_(self):
+        #token
+        TOKEN = ("API_KEY")
+        bot = telebot.TeleBot(TOKEN)        
 
-#loop
-bot.polling()
+        #commands
+        @bot.message_handler(commands=['start','help'])
+        def send_welcome(message):
+            bot.reply_to(message, "opa, bot teste!")
+
+        #loop
+        bot.polling()
+        
+iniciar = main()
+while True:
+    metodo = input('o que você deseja fazer? ')
+     
+    if (metodo == "enviar"):
+        iniciar.send_welcome()
+    
+    if (metodo == "sair"):
+        break
+    
+print('Código Finalizado')
