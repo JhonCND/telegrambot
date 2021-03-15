353602657fdcca37e964cd2101601a13c4f7910d
JhonCND <68496527+JhonCND@users.noreply.github.com>
2021-03-14 21:00:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import telebot
+import requests
+from web import webscra
 
-class main():
-    def _init_(self):
-        #token
-        TOKEN = ("API_KEY")
-        bot = telebot.TeleBot(TOKEN)        
-
-        #commands
-        @bot.message_handler(commands=['start','help'])
-        def send_welcome(message):
-            bot.reply_to(message, "opa, bot teste!")
-
-        #loop
-        bot.polling()
-        
-iniciar = main()
-while True:
-    metodo = input('o que você deseja fazer? ')
-     
-    if (metodo == "enviar"):
-        iniciar.send_welcome()
+#token
+texto = webscra()
+TOKEN = ("1694856172:AAHe3ibvW85P1fUge2v2n4ES3DnJq3mkRuk")
+bot = telebot.TeleBot(TOKEN)
+#commands
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+     bot.reply_to(message, texto)
+'''
+#web scraping
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+    bot.reply_to(message, "opa, bot teste!") '''
     
-    if (metodo == "sair"):
-        break
-    
-print('Código Finalizado')
+#loop
+bot.polling()
