34c598222e007d46b0481a1c0de6a73791868cc2
JhonCND <68496527+JhonCND@users.noreply.github.com>
2021-04-12 16:57:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import telebot
 import requests
-from web import webscra
+from web import webscraping
 
 #token
-texto = webscra()
-TOKEN = ("CHAVE_KEY")
+texto = webscraping()
+TOKEN = ("1694856172:AAHe3ibvW85P1fUge2v2n4ES3DnJq3mkRuk")
 bot = telebot.TeleBot(TOKEN)
 #commands
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
      bot.reply_to(message, texto)
+#loop
+bot.polling()
 '''
 #web scraping
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
     bot.reply_to(message, "opa, bot teste!") '''
-    
-#loop
-bot.polling()
