2890b04af836038601f89b6128c5f24b09ad9afe
JhonCND <68496527+JhonCND@users.noreply.github.com>
2021-04-13 12:33:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import requests
+import emoji
 from web import webscraping
-
 #token
 texto = webscraping()
 TOKEN = ("1694856172:AAHe3ibvW85P1fUge2v2n4ES3DnJq3mkRuk")
@@ -16,4 +16,4 @@ bot.polling()
 #web scraping
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
-    bot.reply_to(message, "opa, bot teste!") '''
+    bot.reply_to(message, "opa, bot teste!") ''' 
